cab895cc419f976ac156240a2ba3203ac54759b4
JAVA-DSA <ronbonnke789@gmail.com>
2024-05-31 00:32:28 +0530

    your message

---
+++ portfolio/src/components/Layout/index.js
@@ -0,0 +1,14 @@
+import Sidebar from '../Sidebar'
+import './index.scss'
+
+import React from 'react'
+
+const Layout = () => {
+  return (
+    <>
+      <Sidebar />
+    </>
+  )
+}
+
+export default Layout
---
+++ portfolio/src/components/Sidebar/index.js
@@ -0,0 +1,16 @@
+import './index.scss'
+import { Link } from 'react-router-dom'
+import React from 'react'
+import LogoS from '../../assets/images/logo-s.png'
+import LogoSubtitle from '../../assets/images/logo_sub.png'
+
+const Sidebar = () => (
+  <div className="nav-bar">
+    <Link className="logo" to="/">
+      <img src={LogoS} alt="logo" />
+      <img className="sub-logo" src={LogoSubtitle} alt="slobodan" />
+    </Link>
+  </div>
+)
+
+export default Sidebar
