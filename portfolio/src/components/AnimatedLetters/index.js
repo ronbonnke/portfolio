b63e610759290b3560896c18c7679abe79626048
JAVA-DSA <ronbonnke789@gmail.com>
2024-06-01 00:19:50 +0530

    your message

--- portfolio/src/components/AnimatedLetters/index.js
+++ portfolio/src/components/AnimatedLetters/index.js
@@ -1,1 +1,15 @@
 import './index.scss'
+
+import React from 'react'
+
+const AnimatedLetters = ({ letterClasss, strArray, idx }) => {
+  return (
+    <span>
+      {strArray.map((char, i) => (
+        <span key={char + i} className="={`${letterClass} _${i+idx}`}"></span>
+      ))}
+    </span>
+  )
+}
+
+export default AnimatedLetters
